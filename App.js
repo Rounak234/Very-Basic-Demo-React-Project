eeb94f69540ea5ddb78fd9066428bace3148bca4
Rounak Poddar <88239475+Rounak234@users.noreply.github.com>
2021-12-28 18:17:06 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,31 @@
+
+import './App.css';
+import Sidebar from './components/Sidebar';
+import SidebarData from './components/SidebarData';
+import Home from './pages/Home'
+
+function App() {
+  return (
+    <div className="App">
+
+      <div className="left">
+        <p>A Demo website</p>
+      </div>
+
+
+      <Sidebar/>
+
+      
+
+    
+    
+    </div>
+    
+    
+  );
+
+
+  
+}
+
+export default App;
